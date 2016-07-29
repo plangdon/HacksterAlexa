9f14469c2597dcf1218cea9f869e29707dfa6988
plangdon <plangdon317@gmail.com>
2016-07-29 07:28:58 -0400

    made generic

--- projects/factSkill-inline/index.js
+++ projects/factSkill-inline/index.js
@@ -228,19 +228,19 @@ var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-u
  * Array containing space facts.
  */
 var FACTS = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+    "Your fact 1",
+    "Your fact 2",
+    "Your fact 3",
+    "Your fact 4",
+    "Your fact 5",
+    "Your fact 6",
+    "Your fact 7",
+    "Your fact 8",
+    "Your fact 9",
+    "Your fact 10",
+    "Your fact 11",
+    "Your fact 12",
+    "Your fact 13"
 ];
 
 /**
@@ -286,7 +286,7 @@ Fact.prototype.intentHandlers = {
     },
 
     "AMAZON.HelpIntent": function (intent, session, response) {
-        response.ask("You can say tell me a space fact, or, you can say exit... What can I help you with?", "What can I help you with?");
+        response.ask("You can say tell me a fact, or, you can say exit... What can I help you with?", "What can I help you with?");
     },
 
     "AMAZON.StopIntent": function (intent, session, response) {
